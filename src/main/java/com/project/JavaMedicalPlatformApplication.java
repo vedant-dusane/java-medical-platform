3aa889ee5dd06209ac8db1a85facd6569f05a89b
Vedant Dusane <vibgroy28@gmail.com>
2026-07-12 18:00:00 +0530

    application

--- src/main/java/com/project/JavaMedicalPlatformApplication.java
+++ src/main/java/com/project/JavaMedicalPlatformApplication.java
@@ -1,4 +1,4 @@
-package com.project.java_medical_platform;
+package com.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
